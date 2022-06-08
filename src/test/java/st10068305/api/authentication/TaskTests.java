dc113aa40477bc6050647a0889509d8bf7e55530
Connor Davis <cnnrproton@gmail.com>
2022-06-08 11:19:54 +0200

    Finish tests

--- src/test/java/st10068305/api/authentication/TaskTests.java
+++ src/test/java/st10068305/api/authentication/TaskTests.java
@@ -11,16 +11,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TaskTests {
-    private int maximumTasks = 2;
-
+    private TaskManager taskManager;
     private Task taskOne;
     private Task taskTwo;
 
     @BeforeEach
     void setup() {
-        TaskManager taskManager = new TaskManager();
+        taskManager = new TaskManager();
 
         taskManager.setTasks(new HashMap<>());
+
+        int maximumTasks = 2;
+
         taskManager.setMaximumTasks(maximumTasks);
 
         taskOne = new Task("Login Feature", taskManager.generateTaskNumber(), "Create Login to authenticate users", "Robyn Harrison", 8, "To Do");
@@ -35,7 +37,6 @@ public class TaskTests {
     @Test
     @Order(1)
     @DisplayName("Test checkTaskDescription()")
-    @Description("Task Description should not be more than 50 Characters")
     void checkTaskDescription() {
         assertEquals(Task.testTaskDescription("This will pass."), "Task successfully captured");
         assertEquals(Task.testTaskDescription("This will not pass as it contains more than the limit of 50 characters."), "Please enter a task description of less than 50 characters");
@@ -43,10 +44,41 @@ public class TaskTests {
 
     @Test
     @Order(2)
-    @DisplayName("Test taskIdCorrect()")
-    @Description("TaskID is correct")
+    @DisplayName("Test testIdCorrect()")
     void testIdCorrect() {
         assertEquals(taskOne.getTaskId(), "LF:0:BYN");
         assertEquals(taskTwo.getTaskId(), "AT:1:IKE");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test testHoursCorrectlyAccumulated()")
+    void testHoursCorrectlyAccumulated() {
+        assertEquals(taskManager.returnTotalHours(), 18);
+
+
+        taskManager.setTasks(new HashMap<>());
+
+        Task taskOne = new Task("Login Feature", taskManager.generateTaskNumber(), "Create Login to authenticate users", "Robyn Harrison", 10, "To Do");
+
+        taskManager.addTask(taskOne);
+
+        Task taskTwo = new Task("Login Feature", taskManager.generateTaskNumber(), "Create Login to authenticate users", "Robyn Harrison", 12, "To Do");
+
+        taskManager.addTask(taskTwo);
+
+        Task taskThree = new Task("Login Feature", taskManager.generateTaskNumber(), "Create Login to authenticate users", "Robyn Harrison", 55, "To Do");
+
+        taskManager.addTask(taskThree);
+
+        Task taskFour = new Task("Login Feature", taskManager.generateTaskNumber(), "Create Login to authenticate users", "Robyn Harrison", 11, "To Do");
+
+        taskManager.addTask(taskFour);
+
+        Task taskFive = new Task("Login Feature", taskManager.generateTaskNumber(), "Create Login to authenticate users", "Robyn Harrison", 1, "To Do");
+
+        taskManager.addTask(taskFive);
+
+        assertEquals(taskManager.returnTotalHours(), 89);
     }
 }
